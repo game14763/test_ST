c330ba662f70e05e2aa9d44bac3b1728682c4a53
Danai <s5901012630059@email.kmutnb.ac.th>
2019-08-16 21:22:36 +0700

    Count indent(s) per news

--- test08.py
+++ test08.py
@@ -8,11 +8,15 @@ tree2 = ET.parse('document.xml')
 file_docx = tree2.getroot()
 
 n = 0
+m = 1
 for i in file_odt.findall(office+"body"):
     for j in i:
         for k in j.findall(text+"p"):
-            if k.text != None:
+            if k.text is not None:
                 #print(k.text)
                 n += 1
-print("All Indentation: " + str(n))
-
+            elif k.findall(text+"a") != []: # Use elif because if it is None  it will pass here instead
+                print("News No.:" + str(m) + "  " + "Indentation: " + str(n))
+                n = 0
+                m += 1
+print("All news count is " + str(m))
