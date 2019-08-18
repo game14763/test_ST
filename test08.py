07e25b83043c4a91235f8151a1f9abd0cefa37b7
Danai <s5901012630059@email.kmutnb.ac.th>
2019-08-18 10:11:35 +0700

    Insert news Rank

--- test08.py
+++ test08.py
@@ -7,16 +7,30 @@ file_odt = tree1.getroot()
 tree2 = ET.parse('document.xml')
 file_docx = tree2.getroot()
 
+
+
 n = 0
 m = 1
-for i in file_odt.findall(office+"body"):
+for i in file_odt.findall(office + "body"):
     for j in i:
-        for k in j.findall(text+"p"):
+        all_p = 0
+        for k in j.findall(text + "p"):
+            all_p += 1
             if k.text is not None:
-                #print(k.text)
+                print(k.text)
                 n += 1
-            elif k.findall(text+"a") != []: # Use elif because if it is None  it will pass here instead
-                print("News No.:" + str(m) + "  " + "Indentation: " + str(n))
+            # Use elif because if it is None  it will pass here instead
+            elif k.findall(text + "a") != []:
+                print("News Rank: " + str(m) + "   " + "Paragraph Count: " + str(n))
+                r = ET.Element(text + "p")
+                r.text = "News Rank: " + str(m) + "    " + "Paragraph Count: " + str(n)
+                r.set(text + "style-name", "Standard")
+                j.insert(all_p, r)
+                all_p += 1
                 n = 0
                 m += 1
-print("All news count is " + str(m))
+
+print("All news count is " + str(m-1))
+ET.dump(tree1)
+
+
